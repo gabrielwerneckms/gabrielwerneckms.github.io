e36387490cc6defbd061a55da100f4bca3ad2332
gabrielwerneckms <39500453+gabrielwerneckms@users.noreply.github.com>
2019-02-15 14:24:05 -0200

    initial smq tool commit

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1550246366,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/levelrectangle.png",
+		"images/arrowhorizontal-sheet0.png",
+		"images/arrowvertical-sheet0.png",
+		"images/type-sheet0.png",
+		"images/workarea-sheet0.png",
+		"images/gridlinehorizontal.png",
+		"images/overlayhud-sheet0.png",
+		"images/hudhandle-sheet0.png",
+		"images/hudhandle2-sheet0.png",
+		"images/selected-sheet0.png",
+		"images/selected-sheet1.png",
+		"images/line-sheet0.png",
+		"images/button-sheet0.png",
+		"images/helptext-sheet0.png"
+	]
+}
