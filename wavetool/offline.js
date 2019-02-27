2139d5a890b7706929f56a3fe90b1808e6692889
gabrielwerneckms <39500453+gabrielwerneckms@users.noreply.github.com>
2019-02-26 22:09:05 -0300

    wave tool

---
+++ wavetool/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1550589764,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/vimageboard-sheet0.png",
+		"images/vimageboard-sheet1.png",
+		"images/vimagebox.png",
+		"images/biggrid.png",
+		"images/levelgrid.png",
+		"images/sprite2-sheet0.png",
+		"images/tilemap.png",
+		"images/vimage-sheet0.png",
+		"images/vimage-sheet1.png",
+		"images/vimage-sheet2.png",
+		"images/portal-sheet0.png",
+		"images/portal-sheet1.png",
+		"images/portal-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
