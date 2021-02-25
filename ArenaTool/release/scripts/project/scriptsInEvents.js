8e592710b39a926c2caa4f22355266de814b809d
gabrielwerneckms <39500453+gabrielwerneckms@users.noreply.github.com>
2021-02-25 18:40:56 -0300

    inimigos da regiao final

---
+++ ArenaTool/release/scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Boardsheet_Event54(runtime, localVars)
+		{
+			window.onbeforeunload = () => {return '';}
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
