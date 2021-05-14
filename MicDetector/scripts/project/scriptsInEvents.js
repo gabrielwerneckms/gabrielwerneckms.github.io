803106996d7bc37f064d3184a3da3ff21a84a7e0
gabrielwerneckms <39500453+gabrielwerneckms@users.noreply.github.com>
2021-05-14 13:59:21 -0300

    Mic Detector

---
+++ MicDetector/scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async EventSheet1_Event4_Act3(runtime, localVars)
+		{
+			window.parent.postMessage(localVars.CurrentVolume,"*");
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
