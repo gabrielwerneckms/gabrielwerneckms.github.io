f7f59334e8ac11901a75b286ecd6df098a500971
gabrielwerneckms <39500453+gabrielwerneckms@users.noreply.github.com>
2022-11-25 13:43:22 -0300

    talknote

---
+++ talknote/scripts/project/scriptsInEvents.js
@@ -0,0 +1,18 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event43(runtime, localVars)
+	{
+		var lines = document.getElementsByClassName("Line");
+		var currentLine = lines[lines.length-1];
+		var pair = runtime.layout.getLayer(0).cssPxToLayer(0,currentLine.scrollHeight)
+		localVars.TextHeight = pair[1];
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
