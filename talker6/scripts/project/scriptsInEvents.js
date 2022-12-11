163d08c45fe72bfed9e12c07a9455e693d402e06
gabrielwerneckms <39500453+gabrielwerneckms@users.noreply.github.com>
2022-12-11 17:18:42 -0300

    asdasdv

---
+++ talker6/scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event58(runtime, localVars)
+	{
+		var line = runtime.objects.LineDiv.getPickedInstances()[0].getElement();
+		localVars.TextHeight = runtime.layout.getLayer(0).cssPxToLayer(0,line.offsetHeight)[1]
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
